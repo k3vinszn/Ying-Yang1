223cc740e7c2a145024831846a3d70be7c5a4add
AleksandrosP38 <alexandrepacheco2000@hotmail.com>
2023-11-09 13:05:04 +0000

    update

--- Assets/Scripts/EnemyShooting.cs
+++ Assets/Scripts/EnemyShooting.cs
@@ -34,7 +34,7 @@ public class EnemyShooting : MonoBehaviour
         float distance = Vector2.Distance(transform.position, player.transform.position);
         Debug.Log(distance);
 
-        if (distance < 10)
+        if (distance < 20)
         {
             timer += Time.deltaTime;
 
