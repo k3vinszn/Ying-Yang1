5b1ae0454f0c37699947e711de43e7dde0a7afa8
k3vinszn <kevinriscado98@gmail.com>
2023-11-09 12:18:15 +0000

    script do inimigo
    script do inimigo tem um delay de 10s ate ativar

---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
+    private GameObject player;
+    private bool canStartShooting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayedStart());
+    }
+
+    IEnumerator DelayedStart()
+    {
+        yield return new WaitForSeconds(10f);
+        // Find the player object with the "Player" tag dynamically
+        player = GameObject.FindGameObjectWithTag("Player");
+        canStartShooting = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canStartShooting)
+            return;
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        Debug.Log(distance);
+
+        if (distance < 10)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > 2)
+            {
+                timer = 0;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
