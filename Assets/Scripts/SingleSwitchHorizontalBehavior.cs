2e72f5d1843621121a255fd04ab5dacaec885c21
AleksandrosP38 <alexandrepacheco2000@hotmail.com>
2023-11-29 11:09:32 +0000

    update botoes

--- Assets/Scripts/SingleSwitchHorizontalBehavior.cs
+++ Assets/Scripts/SingleSwitchHorizontalBehavior.cs
@@ -7,8 +7,47 @@ public class SingleSwitchHorizontalBehavior : MonoBehaviour
     [SerializeField] bool isDoorOpenSwitch;
     [SerializeField] bool isDoorClosedSwitch;
 
+    float switchSizeY;
+    Vector3 switchUpPos;
+    Vector3 switchDownPos;
+    float switchSpeed = 1f;
     bool hasBeenPressed = false;
     static int switchesPressedCount = 0;
+
+    void Awake()
+    {
+        switchSizeY = transform.localScale.y / 2;
+        switchUpPos = new Vector3(transform.position.x, transform.position.y + switchSizeY, transform.position.z);
+        switchDownPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (hasBeenPressed)
+        {
+            MoveSwitchDown();
+        }
+        else
+        {
+            MoveSwitchUp();
+        }
+    }
+
+    void MoveSwitchDown()
+    {
+        if (transform.position != switchDownPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, switchDownPos, switchSpeed * Time.deltaTime);
+        }
+    }
+
+    void MoveSwitchUp()
+    {
+        if (transform.position != switchUpPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, switchUpPos, switchSpeed * Time.deltaTime);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
