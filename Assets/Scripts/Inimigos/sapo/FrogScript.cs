38064f457c72ea16c5d97b4ec503d096c52a8885
k3vinszn <kevinriscado98@gmail.com>
2023-12-25 20:09:06 +0000

    Update FrogScript.cs

--- Assets/Scripts/Inimigos/sapo/FrogScript.cs
+++ Assets/Scripts/Inimigos/sapo/FrogScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class FrogScript : MonoBehaviour
 {
+<<<<<<< HEAD
     private bool isAwake = false;
     public float Range = 10.0f;
     public float PlayerDistance = 8.0f;
@@ -14,9 +15,21 @@ public class FrogScript : MonoBehaviour
         
     }
    public void HandlePlayerDistance()
+=======
+    public float detectionRange = 5f;
+    public LayerMask playerLayer;
+
+    private Transform player;
+    private bool isPlayerDetected = false;
+
+    void Update()
+>>>>>>> parent of 25e9c53 (script sapo por terminar)
     {
-        if (PlayerDistance <= Range)
+        DetectPlayer();
+
+        if (isPlayerDetected)
         {
+<<<<<<< HEAD
             if (isAwake==false) // enemy is not awake
             {
                 animator.SetBool("HasTarget", true); // range detected animation
@@ -33,12 +46,48 @@ public class FrogScript : MonoBehaviour
         {
             animator.SetBool("idlepar", true);
             isAwake = false;
+=======
+            // Perform actions when the player is detected
+            // For example, you can call a method to attack the player
+            AttackPlayer();
+>>>>>>> parent of 25e9c53 (script sapo por terminar)
         }
     }
 
-    IEnumerator Wait2Seconds() // courotine to wait 2 seconds
+    void DetectPlayer()
     {
-        yield return new WaitForSeconds(2.0f);
-   
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, detectionRange, playerLayer);
+
+        // Check if any of the colliders belong to the player
+        foreach (var collider in colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                // Player detected
+                isPlayerDetected = true;
+                player = collider.transform;
+                return;
+            }
+        }
+
+        // Player not detected
+        isPlayerDetected = false;
+        player = null;
+    }
+
+    void AttackPlayer()
+    {
+        // Implement your logic to attack the player here
+        // For example, you can reduce the player's health or perform any other action
+        Debug.Log("Player in range! Attack!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !other.isTrigger) // Updated tag check and added isTrigger check
+        {
+            other.GetComponent<Mover>().TakeDamage(); // Call the TakeDamage method of the player
+            Destroy(gameObject); // Destroy the bullet
+        }
     }
 }
