81f90cc52385534ef61fb03d59e298febe66f5b2
GhostCyder <146454523+GhostCyder@users.noreply.github.com>
2023-12-28 11:53:18 -0100

    Few Changes
    Test on a spike object, removed some unused gameobjects and organized hierarchy

---
+++ Assets/Arte/Spike/Spike.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+
+            // Debug statement to verify the trigger
+            Debug.Log("Player entered the spike trigger!");
+        }
+    }
+}
