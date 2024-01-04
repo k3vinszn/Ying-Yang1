657772e66da86ec860ec36a7e0d055d74b51b396
k3vinszn <kevinriscado98@gmail.com>
2024-01-04 01:39:55 +0000

    sapo fixed
    GIF DO RONALDO A CHORAR

--- Assets/Scripts/Inimigos/sapo/FrogScript.cs
+++ Assets/Scripts/Inimigos/sapo/FrogScript.cs
@@ -7,12 +7,14 @@ public class FrogScript : MonoBehaviour
     private bool isAwake = false;
     public float Range = 4.2f;
     private Animator animator;
+    private GameObject[] players;
     private GameObject player;
+    private float playerDistance;
     public float triggerRange;
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        players = GameObject.FindGameObjectsWithTag("Player");
         animator = GetComponent<Animator>();
     }
 
@@ -24,15 +26,26 @@ public class FrogScript : MonoBehaviour
 
     private void HandlePlayerDistance()
     {
-        float playerDistance = Vector3.Distance(transform.position, player.transform.position);
-        
+
+        float player1Distance = Vector3.Distance(transform.position, players[0].transform.position);
+        float player2Distance = Vector3.Distance(transform.position, players[1].transform.position);
+
+        if (player1Distance <= player2Distance)
+        {
+            player = players[0];
+            playerDistance = player1Distance;
+        }
+        else
+        {
+            player = players[1];
+            playerDistance = player2Distance;
+        }
 
         if (playerDistance <= Range)
         {
             if (!isAwake)
             {
                 animator.SetBool("activated", true);
-                StartCoroutine(Wait2Seconds());
                 animator.SetBool("slapattack", true);
                 isAwake = true;
             }
@@ -47,7 +60,7 @@ public class FrogScript : MonoBehaviour
             {
                 animator.SetBool("activated", true);
                 animator.SetBool("slapattack", false);
-                animator.SetBool("iddle", true);
+                animator.SetBool("iddle", false);
                 isAwake = false;
             }
             else
@@ -91,7 +104,7 @@ public class FrogScript : MonoBehaviour
         if (other.gameObject.CompareTag("Player") && !other.isTrigger)
         {
             other.GetComponent<Mover>().TakeDamage(); // Call the TakeDamage method of the player
-            
+
         }
 
     }
