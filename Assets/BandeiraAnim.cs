7895492db3c173df69c698889ac3fd488014b3f6
k3vinszn <kevinriscado98@gmail.com>
2024-01-17 18:24:15 +0000

    added animations to the checkpoint flag
    -iddle animation to be addle to flag prefab
    -flag now trigger 1 animation for each platers and a separate one when both players cross

---
+++ Assets/BandeiraAnim.cs
@@ -0,0 +1,46 @@
+
+using UnityEngine;
+public class FlagScript : MonoBehaviour
+{
+    private bool player1Crossed = false;
+    private bool player2Crossed = false;
+
+    public Animator flagAnimator;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        string playerName = other.gameObject.name;
+
+        if (playerName.Equals("Player1"))
+        {
+            player1Crossed = true;
+        }
+        else if (playerName.Equals("Player2"))
+        {
+            player2Crossed = true;
+        }
+
+        // will call the right animations after identifying the players
+        CheckFlagStatus();
+    }
+
+    private void CheckFlagStatus()
+    {
+        // if both players triggered the checkpoint, play the animation accordingly
+        if (player1Crossed && player2Crossed)
+        {
+            flagAnimator.SetTrigger("BothPassaram");
+        }
+        // Player 1 crossed the flag
+        else if (player1Crossed)
+        {
+            flagAnimator.SetTrigger("YangPassou");
+        }
+        // Player 2 crossed the flag
+        else if (player2Crossed)
+        {
+            flagAnimator.SetTrigger("YinPassou");
+        }
+    }
+}
+
